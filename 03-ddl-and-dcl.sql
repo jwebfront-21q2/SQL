85e23522f80719c901011a535ef736810ec071ab
남승균 <skyun.nam@gmail.com>
2021-08-05 16:44:13 +0900

    누락분 재푸시

--- 03-ddl-and-dcl.sql
+++ 03-ddl-and-dcl.sql
@@ -37,3 +37,18 @@ SELECT * FROM DBA_USERS WHERE username = 'C##BITUSER';
 GRANT create session TO C##BITUSER;
 
 -- 일반적으로 CONNECT, RESOURCE 롤을 부여하면 일반사용자의 역할 수행 가능
+GRANT connect, resource TO C##BITUSER;
+
+-- Oracle 12 이후로는 임의로 TABLESPACE를 할당 해 줘야 한다.
+ALTER USER C##BITUSER           -- 사용자 정보 수정
+    DEFAULT TABLESPACE USERS    -- 기본 테이블 스페이스를 USERS 에 지정
+    QUOTA UNLIMITED ON USERS;   --  사용 용량 지정
+    
+-- 객체 권한 부여
+-- C##BITUSER 사용자에게 HR.EMPLOYEES를 SELECT 할 수 있는 권한 부여
+GRANT select ON HR.EMPLOYEES TO C##BITUSER;
+-- 객체 권한 회수
+REVOKE select ON HR.EMPLOYEES FROM C##BITUSER;
+GRANT select ON HR.EMPLOYEES TO C##BITUSER;
+-- 전체 권한 부여시 
+-- GRANT all privileges ...
--- practice02.sql
+++ practice02.sql
@@ -30,5 +30,39 @@ ORDER BY MIN(salary) DESC, AVG(salary) ASC;
 SELECT TO_CHAR(MIN(hire_date), 'yyyy-mm-dd day')
 FROM employees;
 
+-- Q7
+SELECT department_id,
+    AVG(salary),
+    MIN(salary),
+    AVG(salary) - MIN(salary)
+FROM employees
+GROUP BY department_id
+    HAVING AVG(salary) - MIN(salary) < 2000
+ORDER BY AVG(salary) - MIN(salary) DESC;
 
+-- Q8
+SELECT job_id,
+    MAX(salary) - MIN(salary) as diff
+FROM employees
+GROUP BY job_id
+ORDER BY diff DESC;
 
+-- Q9
+SELECT manager_id, ROUND(AVG(salary)), 
+    MIN(salary), MAX(salary)
+FROM employees
+WHERE hire_date >= '05/01/01'
+GROUP BY manager_id
+    HAVING AVG(salary) >= 5000
+ORDER BY AVG(salary) DESC;
+
+-- Q10
+SELECT employee_id, salary, 
+    CASE WHEN hire_date <= '02/12/31' THEN '창립멤버'
+        WHEN hire_date <= '03/12/31' THEN '03년 입사'
+        WHEN hire_date <= '04/12/31' THEN '04년 입사'
+        ELSE '상장이후 입사'
+    END optDate,
+    hire_date
+FROM employees
+ORDER BY hire_date;
